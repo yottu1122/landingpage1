10b743ffb3186a7e14d61c873cb5f3a265e1b285
first commit <>
2020-03-05 17:16:25 +0900

    SARD commit

--- js/main.js
+++ js/main.js
@@ -42,35 +42,35 @@ $('#filter a').click(function() {
 /*     Number Count Animations     */
 /***********************************/
 var table = document.getElementById('body');
-if (table) {
-  var a = 0;
-  $(window).scroll(function () {
-    // USE STRICT
-    "use strict";
-    var oTop = $('.stats-box').offset().top - window.innerHeight;
-    if (a == 0 && $(window).scrollTop() > oTop) {
-      $('.counter-value').each(function () {
-        var $this = $(this),
-          countTo = $this.attr('data-count');
-        $({
-          countNum: $this.text()
-        }).animate({
-          countNum: countTo
-        },
-          {
-            duration: 2000,
-            easing: 'swing',
-            step: function () {
-              $this.text(Math.floor(this.countNum));
-            },
-            complete: function () {
-              $this.text(this.countNum);
-              //alert('finished');
-            }
-          });
-      });
-      a = 1;
-    }
-  });
-}
+// if (table) {
+//   var a = 0;
+//   $(window).scroll(function () {
+//     // USE STRICT
+//     "use strict";
+//     var oTop = $('.stats-box').offset().top - window.innerHeight;
+//     if (a == 0 && $(window).scrollTop() > oTop) {
+//       $('.counter-value').each(function () {
+//         var $this = $(this),
+//           countTo = $this.attr('data-count');
+//         $({
+//           countNum: $this.text()
+//         }).animate({
+//           countNum: countTo
+//         },
+//           {
+//             duration: 2000,
+//             easing: 'swing',
+//             step: function () {
+//               $this.text(Math.floor(this.countNum));
+//             },
+//             complete: function () {
+//               $this.text(this.countNum);
+//               //alert('finished');
+//             }
+//           });
+//       });
+//       a = 1;
+//     }
+//   });
+// }
 
